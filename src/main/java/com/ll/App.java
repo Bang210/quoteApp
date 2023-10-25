5020282a20debe4a75a5b70e464d9f6cf2201a69
Bang210 <bangvelope@gmail.com>
2023-10-25 15:56:41 +0900

    목록 기능 추가

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -18,9 +18,13 @@ class App {
             Scanner scanner = new Scanner(System.in);
             String cmd = scanner.next();
             System.out.printf("입력받은 명령어: %s\n", cmd);
+
+            //종료 명령
             if (cmd.equals("종료") == true) {
                 break;
             }
+
+            //등록 명령
             else if (cmd.equals("등록") == true) {
                 System.out.print("명언: ");
                 cmd = scanner.next();
@@ -29,6 +33,15 @@ class App {
                 cmd = scanner.next();
                 writers.add(cmd);
                 System.out.printf("%s번 명언이 등록되었습니다.\n", quotes.size());
+            }
+
+            //목록 명령
+            else if (cmd.equals("목록") == true) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("-----------------------------");
+                for (int i = 0; i < quotes.size(); i++) {
+                    System.out.printf("%d / %s / %s\n", i + 1, writers.get(i), quotes.get(i));
+                }
             }
         }
     }
