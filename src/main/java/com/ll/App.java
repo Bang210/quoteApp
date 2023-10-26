3c642bb03c90cf805612e7f4c7a4fc0e6125013e
Bang210 <bangvelope@gmail.com>
2023-10-26 14:42:24 +0900

    리포멧팅

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -5,20 +5,30 @@ import java.util.List;
 import java.util.Scanner;
 
 class App {
-    List<String> quotes = new ArrayList<>();
-    List<String> writers = new ArrayList<>();
+    //선언
+    static List<String> quotes;
+    static List<String> writers;
     int delNum;
     int crrNum;
+    static Scanner scanner;
+    static String cmd;
+
+    //생성자
+    App() {
+        quotes = new ArrayList<>();
+        writers = new ArrayList<>();
+        scanner = new Scanner(System.in);
+    }
 
     void run() {
+
         //초기 메시지
         System.out.println("==명언 앱==");
 
         while (true) {
             //명령 입력받기
             System.out.print("명령) ");
-            Scanner scanner = new Scanner(System.in);
-            String cmd = scanner.next();
+            cmd = scanner.next();
             System.out.printf("입력받은 명령어: %s\n", cmd);
 
             //종료 명령
@@ -28,56 +38,73 @@ class App {
 
             //등록 명령
             else if (cmd.equals("등록") == true) {
-                System.out.print("명언: ");
-                cmd = scanner.next();
-                quotes.add(cmd);
-                System.out.print("작가: ");
-                cmd = scanner.next();
-                writers.add(cmd);
-                System.out.printf("%s번 명언이 등록되었습니다.\n", quotes.size());
+                register();
             }
 
             //목록 명령
             else if (cmd.equals("목록")) {
-                System.out.println("번호 / 작가 / 명언");
-                System.out.println("-----------------------------");
-                for (int i = 0; i < quotes.size(); i++) {
-                    System.out.printf("%d / %s / %s\n", i + 1, writers.get(i), quotes.get(i));
-                }
+               listup();
             }
 
             //삭제 명령
-            else if (cmd.substring(0, 2).equals("삭제")) {
-                delNum = Integer.parseInt(cmd.substring(6, cmd.length()));
-                if (delNum > 0 && delNum <= quotes.size()) {
-                    quotes.remove(delNum - 1);
-                    writers.remove(delNum - 1);
-                    System.out.printf("%d번 명언이 삭제되었습니다.\n", delNum);
-                } else {
-                    System.out.printf("%d번 명언은 존재하지 않습니다.\n", delNum);
-                }
+            else if (cmd.startsWith("삭제?id=")) {
+                delete();
             }
 
             //수정 명령
-            else if (cmd.substring(0, 2).equals("수정")) {
-                crrNum = Integer.parseInt(cmd.substring(6, cmd.length()));
-                if (crrNum > 0 && crrNum <= quotes.size()) {
-                    System.out.printf("명언(기존): %s\n명언: ", quotes.get(crrNum - 1));
-                    cmd = scanner.next();
-                    quotes.set(crrNum - 1, cmd);
-                    System.out.printf("작가(기존): %s\n작가: ", writers.get(crrNum - 1));
-                    cmd = scanner.next();
-                    writers.set(crrNum - 1, cmd);
-                    System.out.printf("%d번 명언이 수정되었습니다.\n", crrNum);
-                } else {
-                    System.out.printf("%d번 명언은 존재하지 않습니다.\n", crrNum);
-                }
+            else if (cmd.startsWith("수정?id=")) {
+                correct();
             }
 
             //존재하지 않는 명령에 대한 예외처리
             else {
                 System.out.println("유효하지 않은 명령입니다.");
             }
+        }
+    }
+
+    void register() {
+        //등록 명령 수행
+        System.out.print("명언: ");
+        cmd = scanner.next();
+        quotes.add(cmd);
+        System.out.print("작가: ");
+        cmd = scanner.next();
+        writers.add(cmd);
+        System.out.printf("%s번 명언이 등록되었습니다.\n", quotes.size());
+    }
+    void listup() {
+        //목록 명령 수행
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("-----------------------------");
+        for (int i = 0; i < quotes.size(); i++) {
+            System.out.printf("%d / %s / %s\n", i + 1, writers.get(i), quotes.get(i));
+        }
+    }
+    void delete() {
+        //삭제 명령 수행
+        delNum = Integer.parseInt(cmd.substring(6, cmd.length()));
+        if (delNum > 0 && delNum <= quotes.size()) {
+            quotes.remove(delNum - 1);
+            writers.remove(delNum - 1);
+            System.out.printf("%d번 명언이 삭제되었습니다.\n", delNum);
+        } else {
+            System.out.printf("%d번 명언은 존재하지 않습니다.\n", delNum);
+        }
+    }
+    void correct() {
+        //수정 명령 수행
+        crrNum = Integer.parseInt(cmd.substring(6, cmd.length()));
+        if (crrNum > 0 && crrNum <= quotes.size()) {
+            System.out.printf("명언(기존): %s\n명언: ", quotes.get(crrNum - 1));
+            cmd = scanner.next();
+            quotes.set(crrNum - 1, cmd);
+            System.out.printf("작가(기존): %s\n작가: ", writers.get(crrNum - 1));
+            cmd = scanner.next();
+            writers.set(crrNum - 1, cmd);
+            System.out.printf("%d번 명언이 수정되었습니다.\n", crrNum);
+        } else {
+            System.out.printf("%d번 명언은 존재하지 않습니다.\n", crrNum);
         }
     }
 }
